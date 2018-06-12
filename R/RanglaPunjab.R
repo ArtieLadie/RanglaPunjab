9a9997c263e2d2954e2e6b77dca17e6bc0b7c9f5
Artie Ladie <12385598+ArtieLadie@users.noreply.github.com>
2018-06-11 21:29:26 -0400

    Add files via upload

--- R/RanglaPunjab.R
+++ R/RanglaPunjab.R
@@ -6,7 +6,7 @@ PunjabiPalette <- list (
   GoldenTemple2 = c("#7d84cb", "#374890","#c2592e", "#fa5102", "#722416"),
   Pindh = c("#5eb39c", "#1f6562","#2168c2", "#d77e5f", "#5f3e25"),
   Haveli = c("#f4e5d8","#f0b49b","#bb5a40", "#653219", "#19110b"),
-  Haveli2 = c("#ecece9", "#81826c", "#5d5f64", "#69332e", "#5c4e48"),
+  Haveli2 = c("#ecece9", "#81826c", "#5d5f64", "#69332e", "#5c4e48"), 
   AmritsariKulcha = c("#e3e4d9", "#ebdc9c", "#b3340e", "#67140a", "#2a231d"),
   CholeBhature = c("#7cab70", "#d9bf9c", "#a04d05", "#995f7e", "#972107"),
   BiryaniRice = c("#efc04d","#d94520","#527934","#2d1409","#084365"),
@@ -188,6 +188,7 @@ CustomPal <- function(new_pal){
   if (interactive()){
     cherrypickedpalette <- runApp(list(
       ui = fluidPage(
+        theme = "bootstrap.css",
         titlePanel("Cherry Pick Your Own Palette!"),
         sidebarPanel (hr(),
                       selectInput('col', 'Options', new_pal, multiple=TRUE, selectize=FALSE, size = 15)
@@ -195,7 +196,7 @@ CustomPal <- function(new_pal){
         mainPanel(
           h5('Your Cherry-Picked Palette',style = "font-weight: bold;"),
           fluidRow(column(12,verbatimTextOutput("col"))),
-          actionButton("action", label = "I'm Done")
+          actionButton("action", label = "Save My Palette")
         )
       ),#end fluidPage
       server = function(input,output,session){
